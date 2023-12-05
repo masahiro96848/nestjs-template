6688adcb47477129f094ce1182e035fa4525ca47
masahiro96848 <feetwcpic9436@gmail.com>
2023-12-05 23:20:03 +0900

    認証のguardファイルを作成

--- nest_template/src/main.ts
+++ nest_template/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from '@/app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
+  // Cors対応
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost'],
+  })
   await app.listen(3000)
 }
 bootstrap()
