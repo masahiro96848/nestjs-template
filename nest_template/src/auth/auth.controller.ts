dc7f3740a67d6104f717f599fea2d68f8271160b
masahiro96848 <feetwcpic9436@gmail.com>
2023-12-05 23:12:06 +0900

    新規登録のAPIを実装。docker-compose.ymlファイルを修正

--- nest_template/src/auth/auth.controller.ts
+++ nest_template/src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common'
 import { AuthService } from '@/auth/auth.service'
 import { SignInUserDto } from '@/auth/dto/sign-in-user.dto'
+import { SignUpUserDto } from '@/auth/dto/sign-up-user.dto'
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +17,11 @@ export class AuthController {
   @HttpCode(200)
   async signIn(@Body(ValidationPipe) signInUserDto: SignInUserDto) {
     return await this.authService.signIn(signInUserDto)
+  }
+
+  @Post('sign_up')
+  @HttpCode(201)
+  async signUp(@Body(ValidationPipe) signUpUserDto: SignUpUserDto) {
+    return await this.authService.signUp(signUpUserDto)
   }
 }
--- nest_template/src/auth/auth.service.ts
+++ nest_template/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from '@/prisma.service'
 import { SignInUserDto } from '@/auth/dto/sign-in-user.dto'
 import * as bcrypt from 'bcrypt'
 import { ResponseUserType } from '@/types/User'
+import { SignUpUserDto } from '@/auth/dto/sign-up-user.dto'
 
 @Injectable()
 export class AuthService {
@@ -34,6 +35,45 @@ export class AuthService {
       email: user.email,
       createdAt: user.createdAt,
       updatedAt: user.updatedAt,
+    }
+
+    return {
+      user: resUser,
+    }
+  }
+
+  /**
+   * 会員登録
+   * @param signUpUserDto
+   * @returns
+   */
+  async signUp(signUpUserDto: SignUpUserDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: signUpUserDto.email,
+      },
+    })
+    // メールアドレス確認
+    if (!!user)
+      throw new UnauthorizedException(
+        `${signUpUserDto.email} は別のアカウントで使用されています。`,
+      )
+
+    const hashPassword = await bcrypt.hash(signUpUserDto.password, 10)
+    const createdUser = await this.prisma.user.create({
+      data: {
+        name: signUpUserDto.name,
+        email: signUpUserDto.email,
+        password: hashPassword,
+      },
+    })
+
+    const resUser: ResponseUserType = {
+      id: createdUser.id,
+      name: createdUser.name,
+      email: createdUser.email,
+      createdAt: createdUser.createdAt,
+      updatedAt: createdUser.updatedAt,
     }
 
     return {
